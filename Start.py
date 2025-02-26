ab50a70d134a41268b6aff3fe974c8a533e3cf2d
Karina von Felbert <vonfekar@students.zhaw.ch>
2025-02-26 11:12:24 +0100

    Änderung

--- Start.py
+++ Start.py
@@ -8,10 +8,9 @@ st.title("Meine erste Streamlit App")
 # Streamlit über den Text unten direkt in die App - cool!
 """
 Diese App wurde von folgenden Personen entwickelt:
-- Anna Müller (anmu@zhaw.ch)
-- Peter Meier (peme@zhaw.ch)
+- Elena Stevanovic (stevaele@students.zhaw.ch)
+- Karina von Felbert (vonfekar@students.zhaw.ch)
 
 Diese App ist das leere Gerüst für die App-Entwicklung im Modul Informatik 2 (BMLD/ZHAW)
 
-Autor: Samuel Wehrli (wehs@zhaw.ch)
 """
