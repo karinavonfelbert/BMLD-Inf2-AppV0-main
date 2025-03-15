f0ae7fc390f350be9edf1719d7ea6b13fe269448
Elena Stevanovic <stevaele@students.zhaw.ch>
2025-03-15 18:14:14 +0100

    Datenspeicherung versuch

--- pages/2_Verdünnungsrechner-Daten.py
+++ pages/2_Verdünnungsrechner-Daten.py
@@ -2,5 +2,19 @@ import streamlit as st
 
 st.title("Daten des Verdünnungsrechner")
 
-st.write("Diese Seite ist eine Unterseite der Startseite.")
+if 'data_df' not in st.session_state or st.session_state['data_df'].empty:
+    st.info('Keine Daten vorhanden. Berechnen Sie Ihre Verdünnung im Verdünnungsrechner.')
+    st.stop()
 
+data_df = st.session_state['data_df']
+
+data_df = data_df.sort_values('timestamp', ascending=False)
+
+st.dataframe(data_df)
+
+if st.button("Zurück zur Startseite"):
+    st.switch_page("Start.py")
+if st.button("Zum Verdünnungsrechner"):
+    st.switch_page("pages/1_Verdünnungsrechner.py")
+if st.button("Zur Verdünnungsrechner-Grafik"):
+    st.switch_page("pages/3_Verdünnungsrechner-Grafik.py")
