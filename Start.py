705fc35398dc83441073167321a2902d72e7102e
Elena Stevanovic <stevaele@students.zhaw.ch>
2025-03-19 11:14:19 +0100

    update login manager

--- Start.py
+++ Start.py
@@ -6,7 +6,7 @@ from utils.data_manager import DataManager
 from utils.login_manager import LoginManager
 
 # initialize the data manager
-data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD_App_DB")  # switch drive 
+data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD_DemoApp")  # switch drive 
 
 # initialize the login manager
 login_manager = LoginManager(data_manager)
@@ -14,17 +14,6 @@ login_manager.login_register()  # open login/register page
 
 # load the data from the persistent storage into the session state
 data_manager.load_user_data(
-    session_state_key='data_df', 
-    file_name='data.csv', 
-    initial_value = pd.DataFrame(), 
-    parse_dates = ['timestamp']
-    )
-
-# initialize the data manager
-data_manager = DataManager(fs_protocol='webdav', fs_root_folder="BMLD_App_DB")  # switch drive 
-
-# load the data from the persistent storage into the session state
-data_manager.load_app_data(
     session_state_key='data_df', 
     file_name='data.csv', 
     initial_value = pd.DataFrame(), 
